2334fc48b47805f66154c42cb5636bb70fb11c92
Christopher <180734868+Christopher-ctrl-a@users.noreply.github.com>
2025-09-15 03:55:06 -0400

    Adding the CSS/React.js Files

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,16 @@
-/* App.jsx Christopher Esguerra 301483615  09/10/2025*/
+// App.jsx Christopher Esguerra 301483615 September 11, 2025
 
-import './css/App.css'
-import './css/navbar.css'
-import LogoSVG from './assets/Logos.svg'
-import Home from './pages/home'
-import About from './pages/about'
-import Contact from './pages/contact'
-import Project from './pages/project'
-import Service from './pages/service'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+// import { useState } from 'react'
 
+import './App.css'
+import Navbar from './pages/navbar.jsx'
 
 function App() {
+  // const [count, setCount] = useState(0)
 
   return (
-
-
-    <BrowserRouter>
-      <nav>
-        <img className="logo" src={LogoSVG} alt="Logo" />
-        <Link to="/home">Home</Link> |{" "}
-        <Link to="/about">About</Link> |{" "}
-        <Link to="/contact">Contact</Link>  |{" "}
-        <Link to="/project">Projects</Link> |{" "}
-        <Link to="/service">Services</Link>
-      </nav>
-
-
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/project" element={<Project />} />
-        <Route path="/service" element={<Service />} />
-      </Routes>
-    </BrowserRouter>
-
-  );
+      <Navbar />
+  )
 }
-
-
 
 export default App
